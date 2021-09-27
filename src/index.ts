0086e74251cb031e04f14c34f65d393ead0aa865
Adrien Siami <adrien@siami.fr>
2021-09-27 13:46:14 +0200

    Take postcss dir-dependencies into account

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Plugin } from "esbuild";
-import { Plugin as PostCSSPlugin } from "postcss";
+import { Plugin as PostCSSPlugin, Message } from "postcss";
 import {
   ensureDir,
   readFile,
@@ -157,7 +157,9 @@ const postCSSPlugin = ({
         return {
           namespace: isModule ? "postcss-module" : "file",
           path: tmpFilePath,
-          watchFiles: getFilesRecursive(sourceDir),
+          watchFiles: [result.opts.from].concat(
+            getPostCssDependencies(result.messages)
+          ),
           pluginData: {
             originalPath: sourceFullPath
           }
@@ -238,6 +240,18 @@ let idCounter = 0;
  */
 function uniqueId(): string {
   return Date.now().toString(16) + (idCounter++).toString(16);
+}
+
+function getPostCssDependencies(messages: Message[]): string[] {
+  let dependencies = [];
+  for (const message of messages) {
+    if (message.type == "dir-dependency") {
+      dependencies.push(...getFilesRecursive(message.dir));
+    } else if (message.type == "dependency") {
+      dependencies.push(message.file);
+    }
+  }
+  return dependencies;
 }
 
 export default postCSSPlugin;
